6489ca37d2f4671afd6ab05d7df33179a6dcd341
Pedro Paradela <pedroparadeladev@gmail.com>
2022-03-31 20:07:05 -0300

    Mensagem de iniciado/finalizado

--- src/main/java/plteste/core/ExplodirPorco.java
+++ src/main/java/plteste/core/ExplodirPorco.java
@@ -6,15 +6,19 @@ import plteste.events.PigClick;
 
 public final class ExplodirPorco extends JavaPlugin {
 
+    ExplodirPorco plugin = this;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-        launch(this);
+        plugin.getServer().getConsoleSender().sendMessage("§aPlugin 'Explodir Porcos' Iniciado");
+        launch(plugin);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        plugin.getServer().getConsoleSender().sendMessage("§cPlugin 'Explodir Porcos' Finalizado");
         HandlerList.unregisterAll();
     }
 
